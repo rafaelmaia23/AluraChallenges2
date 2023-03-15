aba87b173b1075e9e688b559b9102c78cc20d475
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-15 14:15:59 -0300

    Added class AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AluraChallenges2.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+	{
+
+	}
+}
