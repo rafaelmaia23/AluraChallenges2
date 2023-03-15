9ba4cb22a5bb29e32d874357d36ce94d92cfa2e2
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-15 15:45:12 -0300

    Added AutoMapper to DI

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt => opt
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<IReceitaService, ReceitaService>();
