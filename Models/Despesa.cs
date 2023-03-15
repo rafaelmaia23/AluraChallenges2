0b7b284983912943728a5dc7513cc26908e71516
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-15 15:05:13 -0300

    Added new data model: Despesa

---
+++ Models/Despesa.cs
@@ -0,0 +1,9 @@
+﻿namespace AluraChallenges2.Models;
+
+public class Despesa
+{
+    public Guid Id { get; set; }
+    public string Descricao { get; set; }
+    public double Valor { get; set; }
+    public DateTime Data { get; set; }
+}
