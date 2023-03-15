bc4bf7c3a466d5f4e2f26de893dcb0d7d6ce0033
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-15 15:26:10 -0300

    Adde DataAnnotations to properties for required and key

--- Models/Receita.cs
+++ Models/Receita.cs
@@ -1,10 +1,17 @@
-﻿namespace AluraChallenges2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraChallenges2.Models
 {
     public class Receita
     {
+        [Key]
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public string Descricao { get; set; }
+        [Required]
         public double Valor { get; set; }
+        [Required]
         public DateTime Data { get; set; }
     }
 }
