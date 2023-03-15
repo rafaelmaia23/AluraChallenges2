9733d64ecfdd288d4ce1c02c72644e05a21c5513
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-15 16:22:34 -0300

    Added AutoMapper profile for Receita

---
+++ Profiles/Receitaprofile.cs
@@ -0,0 +1,14 @@
+﻿using AluraChallenges2.Models;
+using AluraChallenges2.Models.Dtos;
+using AutoMapper;
+
+namespace AluraChallenges2.Profiles;
+
+public class Receitaprofile : Profile
+{
+	public Receitaprofile()
+	{
+		CreateMap<UpsertReceitaDto, Receita>();
+		CreateMap<Receita, ReadReceitaDto>();
+	}
+}
