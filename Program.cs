da44ecd81e150d5b2c606384c93c0c8e406e9ca2
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-15 15:19:33 -0300

    Added Receita Service to DI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using AluraChallenges2.Data;
+using AluraChallenges2.Services;
+using AluraChallenges2.Services.IServices;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,8 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IReceitaService, ReceitaService>();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
