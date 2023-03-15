ba9a055a4aad758a09d8b489f4d9916f7596cea8
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-15 14:55:07 -0300

    Added new data model: Receita

---
+++ Models/Receita.cs
@@ -0,0 +1,10 @@
+﻿namespace AluraChallenges2.Models
+{
+    public class Receita
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
