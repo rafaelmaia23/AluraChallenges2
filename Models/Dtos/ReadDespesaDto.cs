7a7133d90dce79a503abc7aab5110a8a38a62641
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-16 16:20:04 -0300

    Added new dto models: UpsertDespesaDto and ReadDespesaDto

---
+++ Models/Dtos/ReadDespesaDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraChallenges2.Models.Dtos;
+
+public class ReadDespesaDto
+{
+    public Guid Id { get; set; }
+    public string Descricao { get; set; }
+    public double Valor { get; set; }
+    public DateTime Data { get; set; }
+}
