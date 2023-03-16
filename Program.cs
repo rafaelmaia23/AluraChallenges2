0ac5a35ba4568fddf8ad40b1f8ad2f1146b5ac59
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-16 16:27:10 -0300

    Added DespesaService to DI

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<IReceitaService, ReceitaService>();
+builder.Services.AddScoped<IDespesaService, DespesaService>();
 
 builder.Services.AddSwaggerGen();
 
