0f09f49fb4be3f31c56e88ede4246bdd73e6b78d
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-16 16:22:03 -0300

    Added AutoMapper profile for Despesa

---
+++ Profiles/DespesaProfile.cs
@@ -0,0 +1,14 @@
+﻿using AluraChallenges2.Models;
+using AluraChallenges2.Models.Dtos;
+using AutoMapper;
+
+namespace AluraChallenges2.Profiles;
+
+public class DespesaProfile : Profile
+{
+	public DespesaProfile()
+	{
+		CreateMap<UpsertDespesaDto, Despesa>();
+		CreateMap<Despesa, ReadDespesaDto>();
+	}
+}
