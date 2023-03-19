3063eb696c71e670716f2504afbb707b4ee090da
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-19 20:34:39 -0300

    Deleted Categoria property de UpsertDespesaDto.

--- Models/Dtos/UpsertDespesaDto.cs
+++ Models/Dtos/UpsertDespesaDto.cs
@@ -6,7 +6,7 @@ public class UpsertDespesaDto
 {
     [Required]
     public string Descricao { get; set; }
-    public Categoria Categoria { get; set; }
+    public int CategoriaId { get; set; }
     [Required]
     public double Valor { get; set; }
     [Required]
