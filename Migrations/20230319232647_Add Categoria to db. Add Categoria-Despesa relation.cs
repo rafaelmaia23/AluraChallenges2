aaf9242cf30c52f189b4cc0545b73598246b8ffe
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-19 20:36:09 -0300

    Added migration for Categoria and Categoria-Despera relation to Db

---
+++ Migrations/20230319232647_Add Categoria to db. Add Categoria-Despesa relation.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AluraChallenges2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoriatodbAddCategoriaDespesarelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriaId",
+                table: "Despesas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Despesas_CategoriaId",
+                table: "Despesas",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Despesas_Categorias_CategoriaId",
+                table: "Despesas",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Despesas_Categorias_CategoriaId",
+                table: "Despesas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Despesas_CategoriaId",
+                table: "Despesas");
+
+            migrationBuilder.DropColumn(
+                name: "CategoriaId",
+                table: "Despesas");
+        }
+    }
+}
