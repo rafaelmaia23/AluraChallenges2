d9bd483819f6b50c20922ff3a76bc5b40efe5926
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-19 21:34:01 -0300

    Changed Categoria property to ReadCategoriaDto

--- Models/Dtos/ReadDespesaDto.cs
+++ Models/Dtos/ReadDespesaDto.cs
@@ -7,7 +7,7 @@ public class ReadDespesaDto
     public Guid Id { get; set; }
     public string Descricao { get; set; }
     public int CategoriaId { get; set; }
-    public Categoria Categoria { get; set; }
+    public ReadCategoriaDto Categoria { get; set; }
     public double Valor { get; set; }
     public DateTime Data { get; set; }
 }
