10a21d120c6164b33d81dabada24a9f9f95847bc
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-19 21:06:10 -0300

    Added migration for data seed of categoria to db

---
+++ Migrations/20230320000317_Add categoria data.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AluraChallenges2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addcategoriadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Outras" },
+                    { 2, "Alimentação" },
+                    { 3, "Saúde" },
+                    { 4, "Moradia" },
+                    { 5, "Transporte" },
+                    { 6, "Educação" },
+                    { 7, "Lazer" },
+                    { 8, "Imprevistos" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
