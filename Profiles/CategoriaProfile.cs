b92841a4d435e9c2d6a00569123891d3a17f4e9e
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-19 21:32:55 -0300

    Added AutoMapper Profile for Categoria

---
+++ Profiles/CategoriaProfile.cs
@@ -0,0 +1,13 @@
+﻿using AluraChallenges2.Models;
+using AluraChallenges2.Models.Dtos;
+using AutoMapper;
+
+namespace AluraChallenges2.Profiles;
+
+public class CategoriaProfile : Profile
+{
+	public CategoriaProfile()
+	{
+		CreateMap<Categoria, ReadCategoriaDto>();
+	}
+}
