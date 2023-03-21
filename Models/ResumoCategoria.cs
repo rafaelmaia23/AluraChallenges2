193db8fd3b81da2611ad6b704e697e3310d22b09
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-21 16:01:16 -0300

    Added new data models: ResumoMes and resumoCategoria

---
+++ Models/ResumoCategoria.cs
@@ -0,0 +1,7 @@
+﻿namespace AluraChallenges2.Models;
+
+public class ResumoCategoria
+{
+    public string Name { get; set; }
+    public double TotalGasto { get; set; }
+}
---
+++ Models/ResumoMes.cs
@@ -0,0 +1,9 @@
+﻿namespace AluraChallenges2.Models;
+
+public class ResumoMes
+{
+    public double TotalReceitas { get; set; }
+    public double TotalDespesas { get; set; }
+    public double SaldoMes { get; set; }
+    public List<ResumoCategoria> ResumoCategorias { get; set; }
+}
